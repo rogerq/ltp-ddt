6b64639bbfd6ac25c6ae4cce6e18e66450e710cf
Koteswararao Nayudu <koteswararao.n@ti.com>
2011-10-20 11:15:28 +0530

    EDMA: Added am335x and beaglebone platforms to EDMA test suite
    Signed-off-by: Yan Liu <yan-liu@ti.com>

---
+++ testcases/ddt/edma_test_suite/src/kernel/interface/beaglebone/kSt_edma_interface.h
@@ -0,0 +1,26 @@
+/*
+ * kSt_edma_interface.h
+ *
+ * header file accessed by the edma parser
+ *
+ * Copyright (C) 2009 Texas Instruments Incorporated - http://www.ti.com/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation version 2.
+ *
+ * This program is distributed <93>as is<94> WITHOUT ANY WARRANTY of any
+ * kind, whether express or implied; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _KST_EDMA_INTERFACE_H_
+#define _KST_EDMA_INTERFACE_H_
+
+//#include <asm/hardware/edma.h>
+#include <mach/edma.h>
+
+#endif  /*_KST_EDMA_INTERFACE_H_*/
+
+
