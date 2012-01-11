0743b8c71bb49628078b9f43ffdc8c088abdf5cb
Koteswararao Nayudu <koteswararao.n@ti.com>
2012-01-11 16:03:25 +0530

    Updated get_modular_config_names.sh script for running the usb modular read/write testcases on AM335x
    Signed-off-by: Yan Liu <yan-liu@ti.com>

--- testcases/ddt/scripts/get_modular_config_names.sh
+++ testcases/ddt/scripts/get_modular_config_names.sh
@@ -111,7 +111,8 @@ case $MACHINE in
 	am3517-evm)
 		sound='CONFIG_SND_OMAP_SOC_AM3517EVM:snd-soc-am3517evm';;
 	am335x-evm)
-	        sound='CONFIG_SND_SOC_TLV320AIC3X:snd-soc-tlv320aic3x CONFIG_SND_AM335X_SOC_EVM:snd-soc-evm CONFIG_SND_DAVINCI_SOC_MCASP:snd-soc-davinci-mcasp CONFIG_SND_AM33XX_SOC:snd-soc-davinci';;	
+	        sound='CONFIG_SND_SOC_TLV320AIC3X:snd-soc-tlv320aic3x CONFIG_SND_AM335X_SOC_EVM:snd-soc-evm CONFIG_SND_DAVINCI_SOC_MCASP:snd-soc-davinci-mcasp CONFIG_SND_AM33XX_SOC:snd-soc-davinci'
+		usb= 'CONFIG_USB_MUSB_TI81XX_GLUE:ti81xx CONFIG_USB_ETH:g_ether CONFIG_USB_MASS_STORAGE:g_mass_storage';;
 	am180x-evm|am181x-evm|da850-omapl138-evm)
 		sound='CONFIG_SND_DA850_SOC_EVM:snd-soc-evm';;
 	dm6467-evm)
