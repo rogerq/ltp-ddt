5dc92f871129e9f88e7bd533c8d192dcaa68cfa4
Yan Liu <yan-liu@ti.com>
2012-02-24 10:43:29 -0500

    run_processes.sh: modify the way to check the return code of individual process and also pipe stderr to stdout

--- testcases/ddt/scripts/run_processes.sh
+++ testcases/ddt/scripts/run_processes.sh
@@ -9,42 +9,31 @@ i=0
 OFS=$IFS
 IFS="#"
 
-echo "0" > ./$$  # Delete previous content
-export retFile="$$"
-
 # Start processes
 for w in $1 
 do
-  `eval "$w" > ./log$i.tmp; echo "$?" >> ./$retFile` &
+  eval "$w" > ./log$i.tmp 2>&1 &
   pids="$pids:$!"
   i=`expr $i + 1`
 done
 
-echo "Debug: pids=$pids"
-
 IFS=':'
 
-# Wait for all process to complete
+# Wait for all process to complete and check return value of process
 for p in $pids
 do
   if [ "x$p" != "x" ]
   then
-    wait $p
+    wait ${p}
+    rc=$?
+    if [ "$rc" -ne "0" ]
+    then
+      RET=1
+      break
+    fi
   fi
 done
 
-# Check return value
-while read line
-do
-  if [ "$line" != "0" ]
-  then
-    RET=1     
-    break
-  fi
-done < ./"$$"
-
-# Cleanup
 IFS=$OFS
-rm ./$$
 exit $RET
 
