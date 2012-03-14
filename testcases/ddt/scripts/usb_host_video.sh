53709d6e1ba6b40e8b13273769618fef797e6028
Koteswararao Nayudu <koteswararao.n@ti.com>
2012-03-14 15:26:48 +0530

    Added USB Video test cases
    Signed-off-by: Yan Liu <yan-liu@ti.com>

---
+++ testcases/ddt/scripts/usb_host_video.sh
@@ -0,0 +1,95 @@
+#! /bin/sh
+#
+# Copyright (C) 2011 Texas Instruments Incorporated - http://www.ti.com/
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation version 2.
+#
+# This program is distributed "as is" WITHOUT ANY WARRANTY of any
+# kind, whether express or implied; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+source "common.sh"
+source "st_log.sh"
+source "usb_video_search_devices.sh"
+
+############################# Functions #######################################
+usage()
+{
+	cat <<-EOF >&2
+	usage: ./${0##*/} [-w WIDTH] [-l LENGTH] [-i FILE_NAME] [-h HELP]
+	-w WIDTH          	Video Resolution Width
+	-l LENGTH        	Video Resolution Length
+	-i FILE_NAME       	File Name
+	-h HELP          	Help
+	EOF
+	exit 0
+}
+
+################################ CLI Params ####################################
+# Please use getopts
+while getopts  :w:l:i:h arg
+do case $arg in
+        w)      WIDTH="$OPTARG";;
+        l)      LENGTH="$OPTARG";;
+        i)      FILE_NAME="$OPTARG";;
+        h)      HELP="$OPTARG";;
+        :)      die "$0: Must supply an argument to -$OPTARG.";;
+        \?)     die "Invalid Option -$OPTARG ";;
+esac
+done
+
+############################ Default Values for Params ###############################
+
+: ${DEV_NAME:='/dev/video0'}
+: ${FILE_NAME:='/home/root/capture.YUV'}
+: ${WIDTH:='320'}
+: ${LENGTH:='240'}
+
+############################ USER-DEFINED Params ###############################
+# Try to avoid defining values here, instead see if possible
+# to determine the value dynamically. ARCH, DRIVER, SOC and MACHINE are
+# initilized and exported by runltp script based on platform option (-P)
+case $ARCH in
+esac
+case $DRIVER in
+esac
+case $SOC in
+esac
+case $MACHINE in
+esac 
+
+########################### REUSABLE TEST LOGIC ###############################
+
+# DO NOT HARDCODE any value. If you need to use a specific value for your setup
+# use USER-DEFINED Params section above.
+
+
+#initalize
+
+DEV_NAME=`Video_Search_Device()`
+
+# Print the test params.
+
+test_print_trc " ****************** TEST PARAMETERS ******************"
+test_print_trc " DEVICE_NAME		:$DEV_NAME"
+test_print_trc " RES_WIDTH		:$WIDTH"
+test_print_trc " RES_LENGTH		:$LENGTH"
+test_print_trc " FILE_NAME  		:$FILE_NAME"
+test_print_trc " *************** END OF TEST PARAMETERS ***************"
+
+#capturing the video
+
+do_cmd cd /opt/ltp/testcases/bin/ddt
+do_cmd ./usb_video_capture -d /dev/$DEV_NAME -w $WIDTH -l $LENGTH -i $FILE_NAME
+
+
+test_print_trc " :: ########################################################## ::"
+test_print_trc " ::                                                            ::"
+test_print_trc " ::                                                            ::"
+test_print_trc " :: Captured video is saved at $FILE_NAME.                     ::"
+test_print_trc " ::                                                            ::"
+test_print_trc " ::                                                            ::"
+test_print_trc " :: ########################################################## ::"
---
+++ testcases/ddt/scripts/usb_video_search_devices.sh
@@ -0,0 +1,52 @@
+#! /bin/sh
+#
+# Copyright (C) 2011 Texas Instruments Incorporated - http://www.ti.com/
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation version 2.
+#
+# This program is distributed "as is" WITHOUT ANY WARRANTY of any
+# kind, whether express or implied; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+
+# search for USB Video devices
+
+source "common.sh"
+source "st_log.sh"
+
+#
+# Search for an USB Video device
+#
+
+Video_Search_Device()
+{
+test_print_trc " ::"
+test_print_trc " :: Determining for video node associated with attached USB camera"
+test_print_trc " ::"
+
+videodev=none
+devices=`ls -d /sys/class/video4linux/video*`
+for device in $devices
+	do
+		module=`egrep usb $device'/device/modalias' | cut -c1-3`
+		if test "$module" = "usb";
+		then
+			videodev=`basename "$device"`
+			camname=`cat $device'/name'`
+			test_print_trc " ::"
+			test_print_trc " :: Found USB Camera: [$camname] mapped to [$videodev]"
+			test_print_trc " ::"
+			echo $videodev;
+		fi
+done
+if test "$videodev" = "none";
+then
+	test_print_trc " ::"
+	test_print_trc " :: No USB Camera found. Exiting Video tests..."
+	test_print_trc " ::"
+	exit 2	
+fi
+}
