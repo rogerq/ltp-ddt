8ce37d3285cc528354463eb00a3d99d7a18aa736
Carlos Hernandez <ceh@ti.com>
2012-07-24 16:00:05 -0400

    Fix USB Audio playback/capture tests
    Previous implementation assumed that alsa devices could record and
    playback which is not correct as some devices might only be capable
    of recording or playing but not both.
    
    * Change usb_audio_search_devices.sh to return card number based on
    desired operation record or playback.
    * Change capture and playback tests defined in test suite
    runtest/ddt/usbhost_audio to specify desired audio operation.
    
    Signed-off-by: Carlos Hernandez <ceh@ti.com>
    Signed-off-by: Yan Liu <yan-liu@ti.com>

--- testcases/ddt/scripts/usb_audio_search_devices.sh
+++ testcases/ddt/scripts/usb_audio_search_devices.sh
@@ -26,33 +26,19 @@ source "st_log.sh"
 
 ############# Do the work ###########################################
 
-#
-# Search for an USB Audio device
-#
-#Audio_Search_Device()
-#{
-#test_print_trc " ::"
-#test_print_trc " :: Determining audio card number for USB Audio device"
-#test_print_trc " ::"
-
-audiodev=none
-devices=`ls -d /sys/class/sound/card*`
-for device in $devices
-	do
-		module=`egrep usb $device'/device/modalias' | cut -c1-3`
-		if test "$module" = "usb";
-		then
-			audiodev=`cat $device'/number'`
-#			test_print_trc " ::"
-#			test_print_trc " :: Found USB Audio device mapped to [card$audiodev]"
-#			test_print_trc " ::"
-			echo $audiodev
-		fi
-	done
-if test "$audiodev" = "none";
+cmd="aplay -l"
+if [ "$1" = "record" ];
 then
-	test_print_trc " ::"
-	test_print_trc " :: No USB Audio device found. Exiting Audio tests..."
-	test_print_trc " ::"	
+  cmd="arecord -l"
 fi
-#}
+
+audiodev=`$cmd | egrep "^card\s+[[:digit:]]+.+USB\s+Audio"| head -n 1 | cut -d':' -f 1 | cut -d' ' -f 2`
+if [ "$audiodev" = "" ]; then
+	test_print_trc " ::"
+	test_print_trc " :: No USB Audio $1 device found. Exiting Audio tests..."
+	test_print_trc " ::"	
+ 	exit 1 
+else
+  echo $audiodev
+fi
+
