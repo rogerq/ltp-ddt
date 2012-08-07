a9c32875e5d0fa5f2fb9ecf4280bf8ab193b1535
Vaidya, Deepti <deepti.vaidya@ti.com>
2012-08-07 10:22:32 +0530

    Add gstreamer tests for different resolutions.
    This patch adds more gstreamer tests for testing streams of resolutions
    WQVGA and WVGA apart from 480p. Also, mpeg4+aac streams have been added.
    This patch provides arguments for specifying resolution, file to be downloaded
    from smruti.india.ti.com and the codec type.
    
    Signed-off-by: Vaidya, Deepti <deepti.vaidya@ti.com>
    Signed-off-by: Yan Liu <yan-liu@ti.com>

--- testcases/ddt/scripts/gst.sh
+++ testcases/ddt/scripts/gst.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-#
+# 
 # Copyright (C) 2011 Texas Instruments Incorporated - http://www.ti.com/
-#
+#  
 # This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as
+# modify it under the terms of the GNU General Public License as 
 # published by the Free Software Foundation version 2.
-#
+# 
 # This program is distributed "as is" WITHOUT ANY WARRANTY of any
 # kind, whether express or implied; without even the implied warranty
 # of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-#
+# 
 
 # Test video functionality using gstreamer
 # Input: File to be downloaded and test file name
@@ -19,27 +19,77 @@
 source "st_log.sh"
 source "common.sh"
 
-DOWNLOAD=$1
-CODEC=$2
+############################# Functions #######################################
+usage()
+{
+cat <<-EOF >&2
+        usage: ./${0##*/} [-d DOWNLOAD] -c <CODEC> -r <RESOLUTION>
+        -d DOWNLOAD	stream to be downloaded from smruti.india.ti.com
+        -c CODEC 	codec h264, mpeg4, mpeg2, mpeg4aac
+	-r RESOLUTION	resolution of stream
+        -h Help         print this usage
+EOF
+exit 0
+}
+############################ Script Variables ##################################
+# Define default valus if possible
 
+############################ CLI Params ###########################################
+while getopts :d:c:r: arg
+do case $arg in
+        d)
+                DOWNLOAD=$OPTARG ;;
+        c)
+                CODEC=$OPTARG ;;
+        r)
+                RESOLUTION=$OPTARG ;;
+        \?)
+		echo "Invalid Option -$OPTARG ignored." >&2
+                usage
+                exit 1
+                ;;
+esac
+done
+
+############################ USER-DEFINED Params ###############################
 case $CODEC in
    264)
-      FILE="HistoryOfTI-480p.264";;
+      FILE="/usr/share/ti/video/HistoryOfTI-$RESOLUTION.264"
+      GSTCMD='gst-launch-0.10 filesrc location="$FILE" ! h264parse ! ffdec_h264 !\
+		 ffmpegcolorspace ! fbdevsink device=/dev/fb';;
    mpeg2)
-      FILE="HistoryOfTI-480p.m2v";;
+      FILE="/usr/share/ti/video/HistoryOfTI-$RESOLUTION.m2v"
+      GSTCMD='gst-launch-0.10 filesrc location="$FILE" ! mpegvideoparse ! \
+		ffdec_mpeg2video ! ffmpegcolorspace ! fbdevsink device=/dev/fb';;
    mpeg4)
-      FILE="HistoryOfTI-480p.m4v";;
+      FILE="/usr/share/ti/video/HistoryOfTI-$RESOLUTION.m4v"
+      GSTCMD='gst-launch-0.10 filesrc location="$FILE" ! mpeg4videoparse ! \
+		ffdec_mpeg4 ! ffmpegcolorspace ! fbdevsink device=/dev/fb';;
+   mpeg4aac)
+      FILE="/usr/share/ti/video/HistoryOfTIAV-$RESOLUTION.mp4"
+      GSTCMD='gst-launch-0.10 filesrc location="$FILE" ! qtdemux name=demux demux.audio_00 \
+		! faad ! alsasink sync=false demux.video_00 ! queue ! ffdec_mpeg4 ! \
+		ffmpegcolorspace ! fbdevsink device=/dev/fb';;
 esac
-echo $FILE
-if [ -f "/usr/share/ti/video/$FILE" ];
-then
-   echo "Demo file exists"
-   `cp /usr/share/ti/video/"$FILE" testfile`
+
+########################### DYNAMICALLY-DEFINED Params #########################
+# Try to use /sys and /proc information to determine values dynamically.
+# Alternatively you should check if there is an existing script to get the
+# value you want
+
+########################### REUSABLE TEST LOGIC ###############################
+if [ -f "$FILE" ];then
+   echo "Demo file $FILE exists"
+else
+   FILE="/usr/share/ti/video/$DOWNLOAD"
+   echo "Running $FILE"
+   if [ ! -s "$FILE" ];then
+      echo "Downloading stream $DOWNLOAD from SMRUTI"
+      do_cmd 'wget http://smruti.india.ti.com/streams/download/"$DOWNLOAD" -O "/usr/share/ti/video/$DOWNLOAD"'\
+ || die "Stream not available!"
+   fi
 fi
 
-if [ ! -s testfile ]
-then
-   echo "Downloading stream from SMRUTI"
-   `wget http://smruti.india.ti.com/streams/download/"$DOWNLOAD" -O testfile`
-fi
+do_cmd "$GSTCMD"
+
 
