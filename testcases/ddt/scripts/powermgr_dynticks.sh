d5ace708fe32df26c9c60a0e7d17bfcd68ee0c3f
Deepti Vaidya <deepti@ubuntu-psp-linux.(none)>
2012-12-20 10:21:42 +0530

    Minor fix in power management dynamic ticks script.
    The option to include math library has been removed while using bc.
    
    Signed-off-by: Deepti Vaidya <deepti@ubuntu-psp-linux.(none)>
    Signed-off-by: Yan Liu <yan-liu@ti.com>

--- testcases/ddt/scripts/powermgr_dynticks.sh
+++ testcases/ddt/scripts/powermgr_dynticks.sh
@@ -7,14 +7,13 @@ VAR10=`cat /proc/interrupts | grep 'timer' | awk -F '[:I]' '{print $2}'`
 echo $VAR10
 EXP=`zcat /proc/config.gz | grep 'CONFIG_HZ' | awk -F '[:=]' '{print $2}'`
 echo $EXP
-EXP=`echo "($EXP * 10)" | bc -l`
-EXPPER=`echo "($EXP * 0.1)" | bc -l` #Calculating 10% of expected increase
+EXP=`echo "($EXP * 10)" | bc`
+EXPPER=`echo "($EXP * 0.1)" | bc` #Calculating 10% of expected increase
 ACTPER=`expr $VAR10-$VAR`
 
 echo "The difference is $ACTPER"
 
-if [ `expr $ACTPER '<' $EXPPER` ]; then
-        exit 0
-		fi
-
-		exit 1
+if [ $ACTPER '<' $EXPPER ]; then
+   exit 0
+fi
+exit 1
