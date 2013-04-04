0ebb91f1ab2b4c98f398074908a69f7f2756ed02
Yan Liu <yan-liu@ti.com>
2013-04-04 11:27:25 -0400

    Add sk and bone to get_config table

--- testcases/ddt/scripts/get_modular_config_names.sh
+++ testcases/ddt/scripts/get_modular_config_names.sh
@@ -118,7 +118,7 @@ case $MACHINE in
 		graphics='CONFIG_PANEL_DVI:panel_dvi CONFIG_PANEL_GENERIC_DPI:panel_generic_dpi CONFIG_FB_OMAP2:omapfb';; 
 	am3517-evm)
 		sound='CONFIG_SND_OMAP_SOC_AM3517EVM:snd-soc-am3517evm';;
-	am335x-evm)
+	am335x-evm|am335x-sk|beaglebone)
 	        sound='CONFIG_SND_SOC_TLV320AIC3X:snd-soc-tlv320aic3x CONFIG_SND_AM335X_SOC_EVM:snd-soc-evm CONFIG_SND_DAVINCI_SOC_MCASP:snd-soc-davinci-mcasp CONFIG_SND_AM33XX_SOC:snd-soc-davinci'
 		usb='CONFIG_USB_MUSB_TI81XX_GLUE:ti81xx';;
 	am180x-evm|am181x-evm|da850-omapl138-evm)
