ee8a252da21038917efe773ea003e6c3817795bc
Yan Liu <yan-liu@ti.com>
2013-04-09 16:45:12 -0400

    powermgr_cpufreq: Add regulator for omap5-evm

--- testcases/ddt/scripts/powermgr_cpufreq.sh
+++ testcases/ddt/scripts/powermgr_cpufreq.sh
@@ -12,11 +12,11 @@
 # GNU General Public License for more details.
 ###############################################################################
 
-# @desc	set SoC to various available OPPs (option for delay & loop), passing suitable options
-# @params d) DELAY	delay between OPP switching
-#         e) EXTREME	set to true if switching required between extreme OPPs
-#	  r} REVERSE	set to true if switching required in descending order of OPPs
-#	  l) LOOP	number of loops to switch between available OPPs
+# @desc set SoC to various available OPPs (option for delay & loop), passing suitable options
+# @params d) DELAY delay between OPP switching
+#         e) EXTREME set to true if switching required between extreme OPPs
+#   r} REVERSE set to true if switching required in descending order of OPPs
+#   l) LOOP number of loops to switch between available OPPs
 # @returns Zero for success, non-zero for failure
 # @history 2012-01-31: First version
 
@@ -27,11 +27,11 @@ usage()
 {
 cat <<-EOF >&2
         usage: ./${0##*/} [-d DELAY] [-e EXTREME] [-r REVERSE] [-l LOOP] 
-	-d DELAY	delay between OPP switching, default is no delay 
-	-e EXTREME	set to true if OPP switching is only required between extreme OPPs, default is to switch between all OPPs
-	-r REVERSE	set to true if OPP switching is required to be descending, default is ascending
-	-l LOOP		number of loops OPP has to be switched, default is 1 loop
-	-h Help         print this usage
+ -d DELAY delay between OPP switching, default is no delay 
+ -e EXTREME set to true if OPP switching is only required between extreme OPPs, default is to switch between all OPPs
+ -r REVERSE set to true if OPP switching is required to be descending, default is ascending
+ -l LOOP  number of loops OPP has to be switched, default is 1 loop
+ -h Help         print this usage
 EOF
 exit 0
 }
@@ -50,7 +50,7 @@ do case $arg in
         d)      DELAY="$OPTARG";;
         e)      extreme="$OPTARG";;
         r)      reverse="$OPTARG";;
-	l)	LOOP="$OPTARG";;
+ l) LOOP="$OPTARG";;
         h)      usage;;
         \?)     die "Invalid Option -$OPTARG ";;
 esac
@@ -61,9 +61,10 @@ REGULATOR="VDD1"
 REGULATOR_VOLT_PATH="/sys/class/regulator/regulator.0/microvolts"
 
 case $MACHINE in
-	am335x-evm) REGULATOR="VDD1";;
-	beaglebone) REGULATOR="DCDC2";;
-	am180x-evm) REGULATOR="VDCDC3";;
+  am335x-evm) REGULATOR="VDD1";;
+  beaglebone) REGULATOR="DCDC2";;
+  am180x-evm) REGULATOR="VDCDC3";;
+  omap5-evm)  REGULATOR="smps123";;
 esac
 
 
@@ -72,12 +73,12 @@ mkfifo temppipe
 ls /sys/class/regulator/ | awk ' {print "/sys/class/regulator/"$1"/name"}' > temppipe &
 while read line
 do
-	CUR_REG=`cat $line`
-	if [ $CUR_REG == $REGULATOR ]
-	then
-		REG_NAME=`echo $line | cut -d '/' -f 5`
-		REGULATOR_VOLT_PATH="/sys/class/regulator/"$REG_NAME"/microvolts"
-	fi
+ CUR_REG=`cat $line`
+ if [ $CUR_REG == $REGULATOR ]
+ then
+  REG_NAME=`echo $line | cut -d '/' -f 5`
+  REGULATOR_VOLT_PATH="/sys/class/regulator/"$REG_NAME"/microvolts"
+ fi
 done < temppipe
 rm temppipe
 
@@ -92,50 +93,50 @@ test_print_trc "OPP to be switched for $LOOP loops with $DELAY seconds delay bet
 while [ $k -lt $LOOP ]
 do
   echo "============ LOOP: $k============"
-	k=$((k+1))
-	if [ "$extreme" == "false" ]
-	then
-		i=0;
-		while [ $i -lt $cnt ]
-		do
-			i=$((i+1))
-			if [ "$reverse" == "false" ]
-			then
-				t=$i
-			else
-				t=`expr $cnt + 1 - $i`
-			fi
-			FREQ=`cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies | awk -v tmp=$t '{print $tmp}'`
-			do_cmd echo $FREQ > "/sys/devices/system/cpu/cpu0/cpufreq/scaling_setspeed" || die "unable to set OPP for $FREQ KHz"
-			FREQUENCY=`cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq`
-			if [ $FREQ -ne $FREQUENCY ]
-			then
-				die "unable to set OPP for $FREQ KHz"
-			fi
-			VOLTAGE=`cat $REGULATOR_VOLT_PATH`
-			test_print_trc "OPP set for: $FREQUENCY KHz $VOLTAGE uV"
-			sleep $DELAY
-		done
-	else
-		FREQ=`cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies | awk '{print $1}'`
-		do_cmd echo $FREQ > "/sys/devices/system/cpu/cpu0/cpufreq/scaling_setspeed" || die "unable to set OPP for $FREQ KHz"
-		FREQUENCY=`cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq`
-		if [ $FREQ -ne $FREQUENCY ]
-		then
-			die "unable to set OPP for $FREQ KHz"
-		fi
-		VOLTAGE=`cat $REGULATOR_VOLT_PATH`
-		test_print_trc "OPP set for: $FREQUENCY KHz $VOLTAGE uV"
-		sleep $DELAY
-		FREQ=`cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies | awk -v tmp=$cnt '{print $tmp}'`
-		do_cmd echo $FREQ > "/sys/devices/system/cpu/cpu0/cpufreq/scaling_setspeed" || die "unable to set OPP for $FREQ KHz"
-		FREQUENCY=`cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq`
-		if [ $FREQ -ne $FREQUENCY ]
-		then
-			die "unable to set OPP for $FREQ KHz"
-		fi
-		VOLTAGE=`cat $REGULATOR_VOLT_PATH`
-		test_print_trc "OPP set for: $FREQUENCY KHz $VOLTAGE uV"
-		sleep $DELAY
-	fi
+  k=$((k+1))
+  if [ "$extreme" == "false" ]
+  then
+  i=0;
+  while [ $i -lt $cnt ]
+  do
+   i=$((i+1))
+   if [ "$reverse" == "false" ]
+   then
+    t=$i
+   else
+    t=`expr $cnt + 1 - $i`
+   fi
+   FREQ=`cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies | awk -v tmp=$t '{print $tmp}'`
+   do_cmd echo $FREQ > "/sys/devices/system/cpu/cpu0/cpufreq/scaling_setspeed" || die "unable to set OPP for $FREQ KHz"
+   FREQUENCY=`cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq`
+   if [ $FREQ -ne $FREQUENCY ]
+   then
+    die "unable to set OPP for $FREQ KHz"
+   fi
+   VOLTAGE=`cat $REGULATOR_VOLT_PATH`
+   test_print_trc "OPP set for: $FREQUENCY KHz $VOLTAGE uV"
+   sleep $DELAY
+  done
+ else
+  FREQ=`cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies | awk '{print $1}'`
+  do_cmd echo $FREQ > "/sys/devices/system/cpu/cpu0/cpufreq/scaling_setspeed" || die "unable to set OPP for $FREQ KHz"
+  FREQUENCY=`cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq`
+  if [ $FREQ -ne $FREQUENCY ]
+  then
+   die "unable to set OPP for $FREQ KHz"
+  fi
+  VOLTAGE=`cat $REGULATOR_VOLT_PATH`
+  test_print_trc "OPP set for: $FREQUENCY KHz $VOLTAGE uV"
+  sleep $DELAY
+  FREQ=`cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies | awk -v tmp=$cnt '{print $tmp}'`
+  do_cmd echo $FREQ > "/sys/devices/system/cpu/cpu0/cpufreq/scaling_setspeed" || die "unable to set OPP for $FREQ KHz"
+  FREQUENCY=`cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq`
+  if [ $FREQ -ne $FREQUENCY ]
+  then
+   die "unable to set OPP for $FREQ KHz"
+  fi
+  VOLTAGE=`cat $REGULATOR_VOLT_PATH`
+  test_print_trc "OPP set for: $FREQUENCY KHz $VOLTAGE uV"
+  sleep $DELAY
+ fi
 done
