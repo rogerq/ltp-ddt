bae7ec628388eae391c5a81c59d03bc7075a2914
Carlos Hernandez <ceh@ti.com>
2013-04-17 15:25:20 -0400

    run_processes.sh: Support CPU Affinity
    Add optional parameters to run multiple instances of each command,
    to assign processes to a group of CPUs and to add inter-process
    delay in seconds.
    Also create temp log files under /tmp directory instead of in
    current directory.
    
    Signed-off-by: Carlos Hernandez <ceh@ti.com>

--- testcases/ddt/scripts/run_processes.sh
+++ testcases/ddt/scripts/run_processes.sh
@@ -2,19 +2,48 @@
 # Execute multiple processes in parallel
 # returns 1 if any process returns non-zero value
 # returns 0 otherwise
+# usage: run_process.sh "#-separated commands" [num instances] 
+#                        [cpu affinity mask] [inter-process start delay]
 
 pids=''
 RET=0
 OFS=$IFS
 IFS="#"
+tmp_dir=`mktemp -d`
+i=0
+
+# Check for number of instances
+p_instances=1
+if [ "x$2" != "x" ]
+then
+  p_instances=$2
+fi
+
+# Check for CPU affinity mask
+p_mask='0xFFFFFFFF'
+if [ "x$3" != "x" ]
+then
+  p_mask=$3
+fi
+
+# Check for inter-process start delay
+p_delay=1
+if [ "x$4" != "x" ]
+then
+  p_delay=$4
+fi
 
 # Start processes
-i=0
 for w in $1 
 do
-  sleep 1
-  eval "$w" > ./log$i.tmp 2>&1 &
-  pids="$pids:$!"
+  for (( j=0 ; j < $p_instances ; j++ ))
+  do 
+    echo "starting $w instance $j" 
+    sleep $p_delay
+    c="taskset $p_mask $w"
+    eval "$c" > $tmp_dir/log$i.$j.tmp 2>&1 &
+    pids="$pids:$!"
+  done
   i=`expr $i + 1`
 done
 cnt=$i
@@ -39,14 +68,17 @@ done
 IFS=$OFS
 
 # Print logs in console
-j=0
-while [ $j -lt $cnt ]
+i=0
+while [ $i -lt $cnt ]
 do
-  echo "*************  log$j.tmp    ***************"
-  cat ./log$j.tmp
-  echo "*************end of log$j.tmp**************"
-  echo ""
-  j=`expr $j + 1`
+  for (( j=0 ; j < $p_instances ;  j++ ))
+  do
+    echo "*************  $tmp_dir/log$i.$j.tmp    ***************"
+    cat $tmp_dir/log$i.$j.tmp
+    echo "*************end of $tmp_dir/log$i.$j.tmp**************"
+    echo ""
+  done
+  i=`expr $i + 1`
 done
 
 exit $RET
