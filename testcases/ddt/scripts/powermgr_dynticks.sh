9862c98bfa2ce67a9093bd05f70ab295386f29cd
Yan Liu <yan-liu@ti.com>
2013-04-19 16:41:35 -0400

    powermgr_dynticks.sh: Fix the script problem

--- testcases/ddt/scripts/powermgr_dynticks.sh
+++ testcases/ddt/scripts/powermgr_dynticks.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-VAR=`cat /proc/interrupts | grep 'timer' | awk -F '[:I]' '{print $2}'`
+VAR=`cat /proc/interrupts | grep 'timer' | awk -F '[:I]' '{print $2}' |awk '{print $1}' |head -1`
 echo $VAR
 sleep 10
-VAR10=`cat /proc/interrupts | grep 'timer' | awk -F '[:I]' '{print $2}'`
+VAR10=`cat /proc/interrupts | grep 'timer' | awk -F '[:I]' '{print $2}'|awk '{print $1}' |head -1`
 echo $VAR10
 EXP=`zcat /proc/config.gz | grep 'CONFIG_HZ' | awk -F '[:=]' '{print $2}'`
 echo $EXP
 EXP=`echo "($EXP * 10)" | bc`
 EXPPER=`echo "($EXP * 0.1)" | bc` #Calculating 10% of expected increase
-ACTPER=`expr $VAR10-$VAR`
+#ACTPER=`expr $VAR10-$VAR`
+ACTPER=$(($VAR10-$VAR))
 
 echo "The difference is $ACTPER"
 
