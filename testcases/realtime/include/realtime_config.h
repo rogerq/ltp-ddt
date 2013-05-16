770a78c17565a7748943919f086092f75719ddae
Elvita Lobo <elvita@ti.com>
2013-05-16 11:26:33 -0400

    Enable rt-kernel build
    Signed-off-by: Yan Liu <yan-liu@ti.com>

---
+++ testcases/realtime/include/realtime_config.h
@@ -0,0 +1,68 @@
+/* include/realtime_config.h.  Generated from realtime_config.h.in by configure.  */
+/* include/realtime_config.h.in.  Generated from configure.in by autoheader.  */
+
+/* Define to 1 if you have PTHREAD_PRIO_INHERIT */
+#define HAS_PRIORITY_INHERIT 1
+
+/* Define to 1 if you have the pthread_mutexattr protocol APIs */
+#define HAS_PTHREAD_MUTEXATTR_PROTOCOL_FUNCTIONS 1
+
+/* Define to 1 if you have pthread_mutexattr_*robust* APIs */
+#define HAS_PTHREAD_MUTEXTATTR_ROBUST_APIS 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <pthread.h> header file. */
+#define HAVE_PTHREAD_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have the __sync_add_and_fetch gcc builtin function */
+#define HAVE___SYNC_ADD_AND_FETCH 1
+
+/* Name of package */
+#define PACKAGE "realtime"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "ltp-results@lists.sourceforge.net"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "realtime"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "realtime realtime-20090930"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "realtime"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "realtime-20090930"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "realtime-20090930"
