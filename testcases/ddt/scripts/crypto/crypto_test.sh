53d79a4672e4087b666675a65f44aae5c4d4de4c
Aparna Balasubramanian <aparnab@ti.com>
2013-11-05 16:41:01 -0600

    Wrapper for crypto tests
    Adding wrapper for crypto tests which use tcrypt and do not provide
    a way of selecting a subset of supported algorithms.
    Corresponding change to test cases is also included here.
    
    Signed-off-by: Aparna Balasubramanian <aparnab@ti.com>
    Signed-off-by: Carlos Hernandez <ceh@ti.com>

---
+++ testcases/ddt/scripts/crypto/crypto_test.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# crypto_test.sh
+# verifies whether log from tcrypt contains only permissible errors
+# usage:  crypto_test.sh 
+
+FILENAME='/var/log/messages'
+fail_flag=0
+searchstring="user.err"
+while read LINE
+do
+  case $LINE in
+
+ *"$searchstring"*) 
+                    case $LINE in
+                    *"lrw"*)  echo "LRW failure";;
+                    *"xts"*) echo "XTS failure";;
+                    *"cfb"*) echo "CFB failure";;
+                    *"ofb"*) echo "OFB failure";;
+                    *"rfc3686"*) echo "RFC3686 failure";;
+                    *) echo "TRUE failure"
+                       echo $LINE
+                       fail_flag=1
+                    ;;
+                    esac
+;;
+ *) ;;
+
+ esac
+done < $FILENAME
+exit $fail_flag
